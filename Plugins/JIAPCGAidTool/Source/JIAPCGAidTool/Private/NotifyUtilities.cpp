1cb99ad9bcf021aa2489a17957c8a790b69a37b8
Epslin <60341743+jiadevr@users.noreply.github.com>
2025-10-30 14:16:59 +0800

    根据[Lessons From a Plugin Developer: How to Extend UE When You’re Not Epic](https://www.youtube.com/watch?v=xilL6Y63QEg)更新角落非阻塞提示框

--- Plugins/JIAPCGAidTool/Source/JIAPCGAidTool/Private/NotifyUtilities.cpp
+++ Plugins/JIAPCGAidTool/Source/JIAPCGAidTool/Private/NotifyUtilities.cpp
@@ -2,7 +2,6 @@
 
 
 #include "NotifyUtilities.h"
-
 #include "Framework/Notifications/NotificationManager.h"
 #include "Widgets/Notifications/SNotificationList.h"
 DEFINE_LOG_CATEGORY(JIAPCGAidTool)
@@ -18,9 +17,20 @@ EAppReturnType::Type UNotifyUtilities::ShowMsgDialog(EAppMsgType::Type MsgType, 
 
 void UNotifyUtilities::ShowPopupMsgAtCorner(const FString& Message)
 {
-	FNotificationInfo NotificationInfo(FText::FromStringView(Message));
+	//主标题设置
+	FNotificationInfo NotificationInfo(INVTEXT("JiaPCGAidTool"));
 	NotificationInfo.bUseLargeFont = true;
+	//文本内容设置
+	NotificationInfo.SubText = FText::FromStringView(Message);
+	//显示时间
+	NotificationInfo.ExpireDuration = 10.0f;
 	NotificationInfo.FadeInDuration = 0.5f;
+	//显示图标
+	NotificationInfo.bUseSuccessFailIcons = false;
+	/*//可选添加按钮,注意最后一个参数必须传入，如果使用SNotificationItem::CS_Pending按钮不会显示
+	NotificationInfo.ButtonDetails.Add(
+		FNotificationButtonInfo(INVTEXT("ButtonText"),INVTEXT("ButtonTip"), FSimpleDelegate(),
+		                        SNotificationItem::CS_None));*/
 	FSlateNotificationManager::Get().AddNotification(NotificationInfo);
 	UE_LOG(JIAPCGAidTool, Display, TEXT("%s"), *Message);
 }
